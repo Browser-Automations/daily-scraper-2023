5c6ef9335f3092a6439e35aa1007fb0c40f387dd
Fahad Altaf <fahadaltaf10SE49@hotmail.com>
2022-12-01 13:11:55 +0400

    Finishing Trulia scraper

---
+++ 01 Dec 2022/trulia-scraper/DataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trulia_scraper
+{
+    public class DataModel
+    {
+        public string PropertyUrl { get; set; }=string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Images { get; set; } = string.Empty;
+        public string Price { get; set; } = string.Empty;
+        public string Beds { get; set; } = string.Empty;
+        public string Baths { get; set; } = string.Empty;
+        public string Area { get; set; } = string.Empty;
+        public string ListedBy { get; set; } = string.Empty;
+    }
+}
--- 01 Dec 2022/trulia-scraper/Program.cs
+++ 01 Dec 2022/trulia-scraper/Program.cs
@@ -3,82 +3,148 @@ using PuppeteerExtraSharp6.Plugins.ExtraStealth;
 using PuppeteerSharp;
 using HtmlAgilityPack;
 using System.Web;
+using trulia_scraper;
 
-//var extra = new PuppeteerExtra();
-//extra.Use(new StealthPlugin());
-//using var browserFetcher = new BrowserFetcher();
-//await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
-//var browser = await extra.LaunchAsync(new LaunchOptions
-//{
-//    Headless = false
-//});
-//var page = (await browser.PagesAsync())[0] ;
-//await page.GoToAsync("https://www.trulia.com/");
-//
-////Have to navigate solw otherwise website will not display any data
-//await page.WaitForTimeoutAsync(3000);
-//
-////search for buy, rent or sold properties
-//await page.ClickAsync("[data-testid='tab-button-buy']");
-////await page.ClickAsync("[data-testid='tab-button-rent']");
-////await page.ClickAsync("[data-testid='tab-button-sold']");
-//
-//await page.TypeAsync("[data-testid='location-search-input']", "33319",new PuppeteerSharp.Input.TypeOptions { Delay=200 });
-//await page.WaitForTimeoutAsync(1500);
-//await page.ClickAsync("[data-testid='location-search-button']");
-//
-//await page.WaitForSelectorAsync("#resultsColumn", new WaitForSelectorOptions { Timeout=30000 });
-//await page.WaitForTimeoutAsync(1500);
-//
-//var html = await page.GetContentAsync();
-var html =File.ReadAllText("data.txt");
-HtmlDocument doc = new HtmlDocument();
-doc.LoadHtml(html);
+List<DataModel> entries = new List<DataModel>();
 
-var totalCountNode = doc.DocumentNode.SelectSingleNode("//*[@id=\"resultsColumn\"]/div[1]/div/div[2]/div/div/div/div[2]/h2");
-if (totalCountNode != null)
+var extra = new PuppeteerExtra();
+extra.Use(new StealthPlugin());
+using var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+var browser = await extra.LaunchAsync(new LaunchOptions
 {
-    Console.WriteLine("Total Results: "+HttpUtility.HtmlDecode(totalCountNode.InnerText));
-}
+    Headless = false
+});
+var page = (await browser.PagesAsync())[0];
+await page.GoToAsync("https://www.trulia.com/");
 
-var listNode = doc.DocumentNode.SelectSingleNode("//ul[@data-testid='search-result-list-container']");
-if (listNode != null)
+//Have to navigate solw otherwise website will not display any data
+await page.WaitForTimeoutAsync(3000);
+
+//search for buy, rent or sold properties
+await page.ClickAsync("[data-testid='tab-button-buy']");
+//await page.ClickAsync("[data-testid='tab-button-rent']");
+//await page.ClickAsync("[data-testid='tab-button-sold']");
+
+await page.TypeAsync("[data-testid='location-search-input']", "33319", new PuppeteerSharp.Input.TypeOptions { Delay = 200 });
+await page.WaitForTimeoutAsync(1500);
+await page.ClickAsync("[data-testid='location-search-button']");
+
+await page.WaitForSelectorAsync("#resultsColumn", new WaitForSelectorOptions { Timeout = 30000 });
+await page.WaitForTimeoutAsync(5000);
+
+int pages = 1;
+for (int i = 1; i <= pages; i++)
 {
-    foreach (var item in listNode.ChildNodes.Where(x=>x.Name=="li"))
+    var html = await page.GetContentAsync();
+    HtmlDocument doc = new HtmlDocument();
+    doc.LoadHtml(html);
+
+    if (i == 1)
     {
-        HtmlDocument recordDoc = new HtmlDocument();
-        recordDoc.LoadHtml(item.InnerHtml);
-
-        var priceNode = doc.DocumentNode.SelectSingleNode("//div[data-testid='property-price']");
-        if (priceNode != null)
+        var totalCountNode = doc.DocumentNode.SelectSingleNode("//*[@id=\"resultsColumn\"]/div[1]/div/div[2]/div/div/div/div[2]/h2");
+        if (totalCountNode != null)
         {
+            int count = Convert.ToInt32(HttpUtility.HtmlDecode(totalCountNode.InnerText).Replace("homes", "").Trim());
 
+            //Website is displaying 40 results per page
+            pages = ((count - 1) / 40) + 1;
+            Console.WriteLine($"Total Results: {count} and Total Pages: {pages}");
         }
+    }
 
-        var bedsNode = doc.DocumentNode.SelectSingleNode("//div[data-testid='property-beds']");
-        if (bedsNode != null)
+    Console.WriteLine($"Extracting data from page {i}");
+    var listNode = doc.DocumentNode.SelectSingleNode("//ul[@data-testid='search-result-list-container']");
+    if (listNode != null)
+    {
+        foreach (var item in listNode.ChildNodes.Where(x => x.Name == "li"))
         {
+            try
+            {
+                DataModel entry = new DataModel();
+                HtmlDocument recordDoc = new HtmlDocument();
+                recordDoc.LoadHtml(item.InnerHtml);
+                var propUrl = recordDoc.DocumentNode.SelectNodes("//a").FirstOrDefault();
+                if (propUrl != null)
+                {
+                    entry.PropertyUrl = propUrl.Attributes["href"].Value;
+                }
 
+                var priceNode = recordDoc.DocumentNode.SelectSingleNode("//div[@data-testid='property-price']");
+                if (priceNode != null)
+                {
+                    entry.Price = HttpUtility.HtmlDecode(priceNode.InnerText);
+                }
+
+                var bedsNode = recordDoc.DocumentNode.SelectSingleNode("//div[@data-testid='property-beds']");
+                if (bedsNode != null)
+                {
+                    entry.Beds = HttpUtility.HtmlDecode(bedsNode.InnerText);
+                }
+
+                var bathNode = recordDoc.DocumentNode.SelectSingleNode("//div[@data-testid='property-baths']");
+                if (bathNode != null)
+                {
+                    entry.Baths = HttpUtility.HtmlDecode(bathNode.InnerText);
+                }
+
+                var areaNode = recordDoc.DocumentNode.SelectSingleNode("//div[@data-testid='property-floorSpace']");
+                if (areaNode != null)
+                {
+                    entry.Area = HttpUtility.HtmlDecode(areaNode.InnerText);
+                }
+
+                var addressNode = recordDoc.DocumentNode.SelectSingleNode("//div[@data-testid='property-address']");
+                if (addressNode != null)
+                {
+                    entry.Address = HttpUtility.HtmlDecode(addressNode.InnerText);
+                }
+
+                var listedByNode = recordDoc.DocumentNode.SelectSingleNode("//div[@data-testid='property-card-listing-attribution-block']");
+                if (listedByNode != null)
+                {
+                    entry.ListedBy = HttpUtility.HtmlDecode(listedByNode.InnerText);
+                }
+
+                var imagesNodes = recordDoc.DocumentNode.SelectNodes("//img");
+                if (imagesNodes.Count > 0)
+                {
+                    List<string> images = new List<string>();
+                    foreach (var node in imagesNodes)
+                    {
+                        var imgUrl = node.Attributes["src"].Value.Replace("thumbs_3", "thumbs_4");
+                        if (!images.Exists(x => x == imgUrl))
+                            images.Add(imgUrl);
+                    }
+                    entry.Images = string.Join(";", images);
+                }
+                entries.Add(entry);
+                Console.WriteLine(entry.Address);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to extract details of a record. Reason: "+ex.Message);
+            }
+            
         }
+    }
 
-        var bathNode = doc.DocumentNode.SelectSingleNode("//div[data-testid='property-baths']");
-        if (bathNode != null)
+    if (i + 1 <= pages)
+    {
+        await page.WaitForTimeoutAsync(3000);
+        Console.WriteLine("Loading next page...");
+        var paginationBtns = doc.DocumentNode.SelectNodes("//li[@data-testid='pagination-page-link']");
+        if (paginationBtns.Count > 0)
         {
-
-        }
-
-        var areaNode = doc.DocumentNode.SelectSingleNode("//div[data-testid='property-floorSpace']");
-        if (areaNode != null)
-        {
-
-        }
-
-        var addressNode = doc.DocumentNode.SelectSingleNode("//div[data-testid='property-address']");
-        if (addressNode != null)
-        {
-
+            var btnToClick = paginationBtns.FirstOrDefault(x => x.InnerText == (i + 1).ToString());
+            if (btnToClick != null)
+            {
+                await (await page.XPathAsync(btnToClick.XPath))[0].ClickAsync();
+                await page.WaitForTimeoutAsync(5000);
+            }
         }
     }
 }
+
 
 
